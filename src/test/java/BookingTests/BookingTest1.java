ed97bbe139ee83bf7e33e7127ed17ec173922a79
gipuk <cubek@o2.pl>
2019-12-16 22:57:02 +0100

    further cihanges

--- src/test/java/BookingTests/BookingTest1.java
+++ src/test/java/BookingTests/BookingTest1.java
@@ -2,26 +2,26 @@ package BookingTests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.Keys;
-
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 
 public class BookingTest1 {
 
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, 20);
 
-    @Test
+    @BeforeAll
 
-    public void startWebDriver() {
+    public void beforeAll() {
 
-        WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, 20);
 
         driver.get("https://phptravels.net");
@@ -38,6 +38,31 @@ public class BookingTest1 {
 
         driver.manage().window().maximize();
 
+        //login to account
+        driver.findElement(By.cssSelector("[class=\"dropdown dropdown-login dropdown-tab\"]")).click();
+        driver.findElement(By.cssSelector("[class=\"dropdown-item active tr\"]")).click();
+        driver.findElement(By.cssSelector("[name=\"username\"]")).sendKeys("user@phptravels.com");
+        driver.findElement(By.cssSelector("[name=\"password\"]")).sendKeys("demouser");
+        driver.findElement(By.cssSelector(".btn-block.loginbtn")).click();
+        driver.get("https://phptravels.net");
+    }
+
+     @AfterAll
+     public void afterAll() {
+        driver.close();
+     }
+
+    @Test
+    public void startWebDriver() {
+
+        //login to account
+        driver.findElement(By.cssSelector("[class=\"dropdown dropdown-login dropdown-tab\"]")).click();
+        driver.findElement(By.cssSelector("[class=\"dropdown-item active tr\"]")).click();
+        driver.findElement(By.cssSelector("[name=\"username\"]")).sendKeys("user@phptravels.com");
+        driver.findElement(By.cssSelector("[name=\"password\"]")).sendKeys("demouser");
+        driver.findElement(By.cssSelector(".btn-block.loginbtn")).click();
+        driver.get("https://phptravels.net");
+
         //select destination
         driver.findElement(By.cssSelector("[id='s2id_autogen1']")).click();
         driver.findElement(By.cssSelector("[id='select2-drop'] .select2-input")).sendKeys("Poznan");
@@ -45,24 +70,25 @@ public class BookingTest1 {
         WebElement obj = wait.until(presenceOfElementLocated(By.cssSelector("ul.select2-result-sub .select2-result-label .select2-match")));
         obj.click();
 
-
         //select dates
         driver.findElement(By.cssSelector("[id='checkin']")).click();
-        driver.findElement(By.cssSelector("[id='checkin']")).sendKeys("15/01/2020");
+        driver.findElement(By.cssSelector("[id='checkin']")).sendKeys("15/02/2020");
         driver.findElement(By.cssSelector("[id=checkout]")).click();
-        driver.findElement(By.cssSelector("[id=checkout]")).sendKeys("19/01/2020");
+        driver.findElement(By.cssSelector("[id=checkout]")).sendKeys("19/02/2020");
+
+        //select 3 Adults
+        driver.findElement(By.cssSelector("[value='2']")).isDisplayed();
+        driver.findElement(By.cssSelector("div.o2 > div > div.form-icon-left > div > span >.bootstrap-touchspin-up")).click();
+
+        //select 1 Child
+        driver.findElement(By.cssSelector("[name='children']")).isDisplayed();
+        driver.findElement(By.cssSelector("div.\\30 1 > div > div > div > span >.bootstrap-touchspin-up")).click();
 
         //click search button
         driver.findElement(By.cssSelector("[class=\"col-md-2 col-xs-12 o1\"]")).click();
 
         //search results page - źle
-        Assert.assertTrue(driver.getTitle().equals("Hotels Listings"));
-
-
-
-        driver.manage().deleteAllCookies();
-        driver.close();
-
+        Assert.assertTrue(driver.getTitle().equals("Hotels Results"));
 
     }
 }
